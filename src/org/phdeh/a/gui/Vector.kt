41ba39f85c14e15643ab709132354b2be4b0ccc3
phdeh <phdeh@icloud.com>
2019-05-29 16:39:37 +0300

    Travis deploy test

--- src/org/phdeh/a/gui/Vector.kt
+++
@@ -1,6 +0,0 @@
-package org.phdeh.a.gui
-
-data class Vector(
-    var x: Double = 0.0,
-    var y: Double = 0.0
-)
--- src/org/phdeh/a/gui/WindowState.kt
+++
@@ -1,8 +0,0 @@
-package org.phdeh.a.gui
-
-enum class WindowState {
-    NONE,
-    SET_VERTEX,
-    MOVE_VERTEX,
-    CONNECT_VERTEX
-}
