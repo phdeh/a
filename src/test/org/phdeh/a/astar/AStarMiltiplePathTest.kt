fe7b39509283a285dd5c85c0b011ac706c84d290
phdeh <phdeh@icloud.com>
2019-05-31 17:33:36 +0300

    Jojo reference.

--- src/test/org/phdeh/a/astar/AStarMiltiplePathTest.kt
+++ src/test/org/phdeh/a/astar/AStarMiltiplePathTest.kt
@@ -1,5 +1,6 @@
 package org.phdeh.a.astar
 
+import org.junit.Ignore
 import org.junit.Test
 
 internal class AStarMiltiplePathTest {
@@ -17,6 +18,23 @@ internal class AStarMiltiplePathTest {
         AStar.findAllPaths(graph["a"], graph["c"]) shouldBe listOf(
             listOf(graph["a"], graph["b"], graph["c"]),
             listOf(graph["a"], graph["d"], graph["c"])
-            )
+        )
     }
+
+    @Ignore("Not implemented yet") @Test
+    fun pathsMadeOnTheRun() {
+        val graph = Graph.build {
+            val a by it(1.0, 0.0)
+            val b by it(0.0, 1.0)
+            val c by it(-1.0, 0.0)
+            val d by it(0.0, -1.0)
+
+            a - b; c - d
+        }
+
+        AStar.findAllPaths(graph["a"], graph["c"]) shouldBe listOf(
+            listOf(graph["a"], graph["e"], graph["c"])
+        )
+    }
+
 }
